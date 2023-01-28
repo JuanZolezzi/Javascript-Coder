4c2da66b2cb5cc1b8574968497dc941279207953
Juan Martin Zolezzi <juanmartinzolezzi@gmail.com>
2023-01-28 02:01:34 -0300

    Proyecto Final

--- app.js
+++
@@ -1,6 +0,0 @@
-import { crearMenu } from "./scripts/menu.js";
-
-const $menuEmpanadas = document.querySelector('#menu-contenedor');
-const API_EMPANADAS = 'https://6310d48c826b98071a4bd630.mockapi.io/empanadas';
-
-crearMenu(API_EMPANADAS);
